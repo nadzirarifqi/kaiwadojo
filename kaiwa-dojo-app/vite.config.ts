c1ce2e9d415d858912ee19d3fb029c1c43328269
Belajar Git <nadzira.rifqi@gmail.com>
2026-09-04 17:11:32 +0700

    Update vite.config.ts

--- kaiwa-dojo-app/vite.config.ts
+++ kaiwa-dojo-app/vite.config.ts
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
+
   plugins: [
     tailwindcss(),
     react(),
